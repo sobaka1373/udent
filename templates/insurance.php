8442be9d1a7a473b304982fc7f4ec2af3e3fc1b8
Vladislav Savitski <vladislav.savitski@innowise-group.com>
2023-02-19 20:36:32 +0300

    insurance page and fix footer and header

--- templates/insurance.php
+++ templates/insurance.php
@@ -1,66 +1,81 @@
 <?php /* Template Name: Insurance */ ?>
 
 <?php get_header(); ?>
-    <div class="insurance-intro">
-        <div class="description">
-            <div class="text">
-                <div class="inner-text">
-                    Стоматология по страховке
-                </div>
-                <div class="first-text">
-                    Добровольное медицинское страхование (ДМС) – это удобный и бесплатный способ получения качественной стоматологической помощи.
-                </div>
-                <div class="first-text">
-                    Для того, чтобы воспользоваться возможностью лечения зубов с помощью ДМС, вам необходимо связаться с вашей страховой компанией или самостоятельно записаться
-                    на прием в Ю-КЛИНИК.
-                </div>
-            </div>
+
+<div class="insurance-page-container">
+  <div class="insurance-data header">
+    <p>
+      Стоматология по страховке
+    </p>
+  </div>
+
+  <div class="insurance-text-container">
+    <div class="insurance-text">
+      <div class="left">
+        <p>
+          Добровольное медицинское страхование (ДМС) — это удобный <br>
+          и бесплатный способ получения качественной стоматологической <br>
+          помощи.
+        </p>
+      </div>
+      <div class="right">
+        <p>
+          Для того, чтобы воспользоваться возможностью лечения зубов <br>
+          с помощью ДМС, вам необходимо связаться с вашей страховой <br>
+          компанией или самостоятельно записаться на прием в Ю-ДЕНТ.
+        </p>
+        <p>
+          Стоматологическая помощь проводится после записи на прием <br>
+          и получения нами гарантийного письма от страховой компании.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="partners-header">
+    <p>
+      Мы сотрудничаем со страховыми компаниями:
+    </p>
+  </div>
+
+  <div class="insurance-container">
+    <div class="row">
+      <div class="service-element">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/insurance1.svg">
+      </div>
+      <div class="service-element">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/insurance2.svg">
+      </div>
+      <div class="service-element">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/insurance3.svg">
+      </div>
+      <div class="service-element">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/insurance4.svg">
+      </div>
+    </div>
+    <div class="row">
+      <div class="service-element">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/insurance5.svg">
+      </div>
+      <div class="service-element">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/insurance6.svg">
+      </div>
+      <div class="service-element">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/insurance7.svg">
+      </div>
+      <div class="service-element">
+        <div class="virtual-btn">
+          <p>подробнее</p>
+          <div class="record-arrow">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/right-arrow.svg">
+          </div>
         </div>
+      </div>
     </div>
-    <div class="insurance-treatment-date">
-        <div class="title">
-            Мы сотрудничаем со следующими страховыми компаниями:
-        </div>
-    </div>
-    <div class="insurance-photo-gallery">
-        <div class="container-photo">
-            <div class="gallery">
-                <figure class="gallery__item gallery__item--1">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/slider/image50.png"
-                         class="gallery__img belstr" alt="Image 1">
-                </figure>
-                <figure class="gallery__item gallery__item--2">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/slider/belveb2.png"
-                         class="gallery__img belveb" alt="Image 1">
-                </figure>
-                <figure class="gallery__item gallery__item--3">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/slider/image33.png"
-                         class="gallery__img prom" alt="Image 1">
-                </figure>
-                <figure class="gallery__item gallery__item--4">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/slider/image49.png"
-                         class="gallery__img task" alt="Image 1">
-                </figure>
-                <figure class="gallery__item gallery__item--5">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/slider/asoba.png"
-                         class="gallery__img asoba" alt="Image 1">
-                </figure>
-                <figure class="gallery__item gallery__item--6">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/slider/image52.png"
-                         class="gallery__img belgtr" alt="Image 1">
-                </figure>
-                <figure class="gallery__item gallery__item--7">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/slider/image48.png"
-                         class="gallery__img gar" alt="Image 1">
-                </figure>
-            </div>
-        </div>
-    </div>
-<div class="last-bk">
-    <div class="first-text">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/kov.png" class="img-pos">
-        Стоматологическая помощь осуществляется после записи на прием и получения нами гарантийного письма от страховой компании.
-    </div>
+  </div>
+
+  <div class="after-footer-row"></div>
+
 </div>
 
 <?php get_footer(); ?>
